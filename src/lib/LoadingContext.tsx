fb1a642ce411fef85e5b74767cf2de7200f10691
computos <computos.riogas@gmail.com>
2025-06-23 09:02:58 -0300

    Cambios recientes

---
+++ src/lib/LoadingContext.tsx
@@ -0,0 +1,20 @@
+// src/lib/LoadingContext.tsx
+'use client';
+import { createContext, useState, useContext } from "react";
+
+const LoadingContext = createContext({
+  isLoading: false,
+  setLoading: (loading: boolean) => {},
+});
+
+export const LoadingProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isLoading, setLoading] = useState(false);
+
+  return (
+    <LoadingContext.Provider value={{ isLoading, setLoading }}>
+      {children}
+    </LoadingContext.Provider>
+  );
+};
+
+export const useLoading = () => useContext(LoadingContext);
