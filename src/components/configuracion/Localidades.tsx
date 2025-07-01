7ecc9effd9c773a5ec13e9a9c1331e8429af309b
computos <computos.riogas@gmail.com>
2025-07-01 10:38:42 -0300

    Componentes de ubicaciones

--- src/components/configuracion/Localidades.tsx
+++ src/components/configuracion/Localidades.tsx
@@ -10,10 +10,10 @@ export default function Localidades() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className="bg-gray-800 text-white p-4 rounded-lg">
+    <div >
       <div className="flex justify-between items-center mb-4">
         <Input placeholder="Buscar localidades..." className="w-1/2 bg-gray-700 text-white" />
-        <Button className="ml-4 bg-blue-500 text-white" onClick={() => setIsModalOpen(true)}>
+        <Button  onClick={() => setIsModalOpen(true)}>
           Importar
         </Button>
       </div>
